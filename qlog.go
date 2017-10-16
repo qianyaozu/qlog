10fca6fdbe4dda1612cc4eedd63f70a762359849
钱耀祖 <262236468@qq.com>
2017-10-16 16:56:38 +0800

    修改log

--- qlog.go
+++ qlog.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"time"
+	"syscall"
 )
 
 type LogStruct struct {
@@ -92,4 +93,33 @@ func Log(level, title string, data interface{}) {
 	log.Level = level
 	log.Data = data
 	LogChannel <- log
+}
+
+
+
+func Panic(f *os.File){
+	redirectStderr(f)
+}
+var (
+	kernel32         = syscall.MustLoadDLL("kernel32.dll")
+	procSetStdHandle = kernel32.MustFindProc("SetStdHandle")
+)
+
+func setStdHandle(stdhandle int32, handle syscall.Handle) error {
+	r0, _, e1 := syscall.Syscall(procSetStdHandle.Addr(), 2, uintptr(stdhandle), uintptr(handle), 0)
+	if r0 == 0 {
+		if e1 != 0 {
+			return error(e1)
+		}
+		return syscall.EINVAL
+	}
+	return nil
+}
+
+// redirectStderr to the file passed in
+func redirectStderr(f *os.File) {
+	err := setStdHandle(syscall.STD_ERROR_HANDLE, syscall.Handle(f.Fd()))
+	if err != nil {
+		log.Fatalf("Failed to redirect stderr to file: %v", err)
+	}
 }
