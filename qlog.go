c9ff7681961250a310b57dd79487e006c182f4c6
qianyaozu <262236468@qq.com>
2019-08-12 13:33:17 +0800

    import elk

--- qlog.go
+++ qlog.go
@@ -1,14 +1,30 @@
 package qlog
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/gin-gonic/gin/json"
 	"github.com/go-redis/redis"
 	"log"
 	"os"
-	"syscall"
+	"sync"
 	"time"
 )
+
+type Options struct {
+	Name         string
+	RedisAddress string
+	ChannelSize  int
+}
+
+var ioMap sync.Map
+
+type QLog struct {
+	Name         string
+	redisClient  *redis.Client
+	redisAddress string
+	logChannel   chan LogStruct
+	run          bool
+}
 
 type LogStruct struct {
 	Title string
@@ -16,125 +32,130 @@ type LogStruct struct {
 	Data  interface{}
 }
 
-var LogChannel = make(chan LogStruct, 10000)
-var pathArray = make([]string, 0)
-var client = redis.NewClient(&redis.Options{
-	Addr:         "192.168.2.207:6379",
-	DialTimeout:  10 * time.Second,
-	ReadTimeout:  30 * time.Second,
-	WriteTimeout: 30 * time.Second,
-	PoolSize:     10,
-	PoolTimeout:  30 * time.Second,
-})
-
-func init() {
-	go HandlerLogs()
+//构造日志对象
+func NewQLog(option *Options) *QLog {
+	qlog := &QLog{
+		Name:         option.Name,
+		run:          true,
+		redisAddress: option.RedisAddress,
+	}
+	if qlog.Name == "" {
+		qlog.Name = "qgate"
+	}
+	if option.RedisAddress != "" {
+		qlog.redisClient = redis.NewClient(&redis.Options{
+			Addr:         option.RedisAddress,
+			DialTimeout:  10 * time.Second,
+			ReadTimeout:  30 * time.Second,
+			WriteTimeout: 30 * time.Second,
+			PoolSize:     10,
+			PoolTimeout:  30 * time.Second,
+		})
+	}
+	if option.ChannelSize <= 0 {
+		option.ChannelSize = 10000
+	}
+	qlog.logChannel = make(chan LogStruct, option.ChannelSize)
+	go qlog.handlerLogs()
+	return qlog
 }
-func HandlerLogs() {
-	for log := range LogChannel {
-		SaveLog(log.Level, log.Title, log.Data)
+
+//结束记录日志
+func (qlog *QLog) Dispose() {
+	qlog.run = false
+}
+
+func (qlog *QLog) handlerLogs() {
+	for {
+		select {
+		case log := <-qlog.logChannel:
+			{
+				qlog.SaveLog(log.Level, log.Title, log.Data)
+				break
+			}
+		case <-time.After(time.Second * 3):
+			{
+				//如果服务已经暂停
+				if !qlog.run {
+					//for k,v:=range ioMap{
+					//	if l,ok:=v.(*log.Logger);strings.Contains(k,".log")&&ok{
+					//
+					//	}
+					//}
+					fmt.Println("log finish")
+				}
+			}
+		}
+	}
+
+	//for log := range qlog.logChannel {
+	//
+	//}
+}
+
+func (qlog *QLog) Error(title string, data interface{}) {
+	qlog.log("Error", title, data)
+}
+func (qlog *QLog) Warn(title string, data interface{}) {
+	qlog.log("Warn", title, data)
+}
+func (qlog *QLog) Info(title string, data interface{}) {
+	qlog.log("Info", title, data)
+}
+func (qlog *QLog) Trace(title string, data interface{}) {
+	qlog.log("Trace", title, data)
+}
+func (qlog *QLog) Fatal(title string, data interface{}) {
+	qlog.log("Fatal", title, data)
+}
+func (qlog *QLog) Elk(title string, data interface{}) {
+	qlog.log("Elk", title, data)
+}
+
+func (qlog *QLog) log(level, title string, data interface{}) {
+	qlog.logChannel <- LogStruct{
+		Title: title,
+		Level: level,
+		Data:  data,
 	}
 }
-func SaveLog(level, title string, data interface{}) {
+
+//处理日志
+func (qlog *QLog) SaveLog(level, title string, data interface{}) {
 	defer func() {
 		if err := recover(); err != nil {
 			log.Println(err)
 		}
 	}()
-	if level == "ELK" {
+	if level == "Elk" && qlog.redisAddress != "" {
 		b, _ := json.Marshal(data)
-		client.RPush("elk_log_queue", string(b)).Result()
+		qlog.redisClient.RPush("elk_log_queue", string(b)).Result()
 		return
 	}
 
-	dir := "log/" + time.Now().Format("20060102") + "/" + level + "/"
-	for _, p := range pathArray {
-		if p == dir {
-			goto saveFile
+	//写入文件
+	dir := "log/" + time.Now().Format("20060102") + "/"
+	if _, ok := ioMap.Load(dir); !ok {
+		//如果不存在目录，则创建一条
+		if _, err := os.Stat(dir); err != nil {
+			if os.MkdirAll(dir, 0777) == nil {
+				ioMap.Store(dir, 1)
+			}
 		}
 	}
 
-	if len(pathArray) > 100 {
-		//清理过多的缓存
-		pathArray = make([]string, 0)
-	}
-	if _, err := os.Stat(dir); err != nil {
-		err = os.MkdirAll(dir, 0777)
+	fileName := dir + title + "_" + level + ".log"
+	logs, ok := ioMap.Load(fileName)
+	if !ok {
+		file, err := os.OpenFile(fileName, os.O_CREATE|os.O_APPEND, 0666)
 		if err != nil {
-			fmt.Println("SaveLog 1", err)
-			return
-		} else {
-			pathArray = append(pathArray, dir)
+			fmt.Println("open file error !", err)
 		}
+		l := log.New(file, "", log.LstdFlags)
+		ioMap.Store(fileName, l)
+		logs = l
 	}
-
-saveFile:
-	fileName := dir + title + ".log"
-	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		fmt.Println("open file error !", err)
-		return
-	}
-	defer file.Close()
-	logs := log.New(file, "", log.LstdFlags)
-	if data != nil {
-		logs.Println(data)
-	} else {
-		fmt.Println("empty", level, title, data)
-	}
-}
-
-func Error(title string, data interface{}) {
-	Log("Error", title, data)
-}
-func Warn(title string, data interface{}) {
-	Log("Warn", title, data)
-}
-func Info(title string, data interface{}) {
-	Log("Info", title, data)
-}
-func Trace(title string, data interface{}) {
-	Log("Trace", title, data)
-}
-func Fatal(title string, data interface{}) {
-	Log("Fatal", title, data)
-}
-func ELK(title string, data interface{}) {
-	Log("ELK", title, data)
-}
-
-func Log(level, title string, data interface{}) {
-	var log LogStruct
-	log.Title = title
-	log.Level = level
-	log.Data = data
-	LogChannel <- log
-}
-
-func Panic(f *os.File) {
-	redirectStderr(f)
-}
-
-var (
-	kernel32         = syscall.MustLoadDLL("kernel32.dll")
-	procSetStdHandle = kernel32.MustFindProc("SetStdHandle")
-)
-
-func setStdHandle(stdhandle int32, handle syscall.Handle) error {
-	r0, _, e1 := syscall.Syscall(procSetStdHandle.Addr(), 2, uintptr(stdhandle), uintptr(handle), 0)
-	if r0 == 0 {
-		if e1 != 0 {
-			return error(e1)
-		}
-		return syscall.EINVAL
-	}
-	return nil
-}
-
-// redirectStderr to the file passed in
-func redirectStderr(f *os.File) {
-	err := setStdHandle(syscall.STD_ERROR_HANDLE, syscall.Handle(f.Fd()))
-	if err != nil {
-		log.Fatalf("Failed to redirect stderr to file: %v", err)
+	if ll, ok := logs.(*log.Logger); ok && data != nil {
+		ll.Println(data)
 	}
 }
